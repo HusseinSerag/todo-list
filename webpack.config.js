050a6576934dd1b3816944c16f8ad0fcf8aa7d1b
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-12-07 22:05:23 +0200

    Add skeleton files
    I created the skeleton files with all the webpack configurations,
    maybe additional configs will be added but for now these are the needed
    ones with scripts for development and for production

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:'./src/scripts/index.js',
+    output:{
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[{
+            test:/\.css$/i,
+            use:['style-loader' , 'css-loader']
+        },{
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+        template:'./src/index.html'
+    })]
+}
